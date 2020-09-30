6cb1f85b320138e8e8e489cf66ec0c64b0939bd4
zili9346 <zili9346@uni.sydney.edu.au>
2020-09-30 12:15:33 +1000

    add jsp file and configuration

--- src/main/java/org/elec5619/peerhelping/controller/UsersController.java
+++ src/main/java/org/elec5619/peerhelping/controller/UsersController.java
@@ -3,18 +3,23 @@ package org.elec5619.peerhelping.controller;
 import org.elec5619.peerhelping.domain.UsersEntity;
 import org.elec5619.peerhelping.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class UsersController {
     @Autowired
     UsersService usersService;
 
+    @ResponseBody
     @GetMapping("/login")
     public UsersEntity userLogin(@RequestParam(value = "sid") String sid,
                                  @RequestParam(value = "pwd") String pwd) {
         return this.usersService.findBySidAndPassword(Integer.parseInt(sid), pwd);
+    }
+
+    @RequestMapping("/")
+    public String loginPage() {
+        return "login";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/dologin.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: john19970623
+  Date: 2020/9/23
+  Time: 11:55 am
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="java.io.*, java.util.*" %>
+<html>
+<head>
+    <title>dologin</title>
+</head>
+<body>
+<h1>redirect</h1>
+
+<script type="text/javascript">
+    window.location.href="https://www.baidu.com"
+</script>
+</body>
+</html>
